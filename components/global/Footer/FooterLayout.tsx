1b5f6fdeb713e396920fb002b4d1dcb7829ff7e2
Marwen Arfa <marwen.arfa@gmail.com>
2024-01-01 04:53:35 +0100

    refactor: add menu items and update footer layout

--- components/global/Footer/FooterLayout.tsx
+++ components/global/Footer/FooterLayout.tsx
@@ -1,7 +1,13 @@
 import type { PortableTextBlock } from '@portabletext/types'
 
 import { CustomPortableText } from '@/components//shared/CustomPortableText'
-import type { SettingsPayload } from '@/types'
+import type { MenuItem, SettingsPayload } from '@/types'
+import { Icons } from '@/components/shared/icons'
+import { ModeToggle } from '@/components/shared/mode-toggle'
+import { buttonVariants } from '@/components/ui/button'
+import { cn } from '@/lib/utils'
+import Link from 'next/link'
+import { MainNav } from '../Navbar/main-nav'
 
 interface FooterProps {
   data: SettingsPayload
@@ -9,14 +15,21 @@ interface FooterProps {
 export default function Footer(props: FooterProps) {
   const { data } = props
   const footer = data?.footer || ([] as PortableTextBlock[])
+  const menuItems = data?.menuItems || ([] as MenuItem[])
   return (
-    <footer className="bottom-0 w-full py-12 text-center md:py-20">
-      {footer && (
-        <CustomPortableText
-          paragraphClasses="text-md md:text-xl"
-          value={footer}
-        />
-      )}
+    <footer className="bottom-0 w-full mt-8 text-center">
+      <div className="container py-4 text-white bg-green-600 md:py-8">
+        <div className="flex flex-col items-center justify-between md:flex-row">
+          
+        <MainNav menuItems={menuItems} className='text-white' />
+        {footer && (
+          <CustomPortableText
+            paragraphClasses="text-white"
+            value={footer}
+          />
+        )}
+        </div>
+      </div>
     </footer>
   )
 }
