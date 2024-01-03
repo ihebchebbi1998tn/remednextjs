33c1106c8fc7152479fd7f0bede009060022cd78
Marwen Arfa <marwen.arfa@gmail.com>
2024-01-03 08:55:00 +0100

    feat: more Demo components

--- components/demos/HeaderDemo.tsx
+++ components/demos/HeaderDemo.tsx
@@ -34,24 +34,6 @@ export default function HeaderDemo() {
       >
         <source src="/videos/omp.mp4" type="video/mp4" />
       </video>
-      <div className="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl">
-        <div
-          className="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#50a066] to-[#776fff] opacity-20"
-          style={{
-            clipPath:
-              'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-          }}
-        />
-      </div>
-      <div className="absolute -top-52 left-1/2 -z-10 -translate-x-1/2 transform-gpu blur-3xl sm:top-[-28rem] sm:ml-16 sm:translate-x-0 sm:transform-gpu">
-        <div
-          className="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#50a066] to-[#776fff] opacity-20"
-          style={{
-            clipPath:
-              'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-          }}
-        />
-      </div>
       <div className="px-6 mx-auto max-w-7xl lg:px-8">
         <div className="max-w-2xl mx-auto lg:mx-0">
           <h2 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
@@ -67,10 +49,10 @@ export default function HeaderDemo() {
           {cards.map((card) => (
             <div
               key={card.name}
-              className="flex p-6 gap-x-4 rounded-xl bg-white/5 ring-1 ring-inset ring-white/10"
+              className="flex p-6 bg-green-500 gap-x-4 rounded-xl ring-1 ring-inset ring-white/10 bg-opacity-80 backdrop-blur-2px"
             >
               <card.icon
-                className="flex-none w-5 text-indigo-400 h-7"
+                className="flex-none w-5 text-yellow-400 h-7"
                 aria-hidden="true"
               />
               <div className="text-base leading-7">
---
+++ components/demos/PostsDemo.tsx
@@ -0,0 +1,119 @@
+import author from "@/sanity/schemas/documents/author"
+
+/* eslint-disable @next/next/no-img-element */
+const posts = [
+  {
+    id: 1,
+    title: 'Boost your conversion rate',
+    href: '#',
+    description:
+      'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel iusto corrupti dicta laboris incididunt.',
+    imageUrl:
+      'https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80',
+    date: 'Mar 16, 2020',
+    datetime: '2020-03-16',
+    category: { title: 'Marketing', href: '#' },
+    author: {
+      name: 'Michael Foster',
+      role: 'Co-Founder / CTO',
+      href: '#',
+      imageUrl:
+        'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    },
+  },
+  {
+    id: 2,
+    title: 'How to use search engine optimization to drive sales',
+    href: '#',
+    description:
+      'Optio cum necessitatibus dolor voluptatum provident commodi et. Qui aperiam fugiat nemo cumque.',
+    imageUrl:
+      'https://images.unsplash.com/photo-1547586696-ea22b4d4235d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3270&q=80',
+    date: 'Mar 10, 2020',
+    datetime: '2020-03-10',
+    category: { title: 'SEO', href: '#' },
+    author: {
+      name: 'Dries Vincent',
+      role: 'SEO Specialist',
+      href: '#',
+      imageUrl:
+        'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    },
+  },
+]
+
+export default function PostsDemo() {
+  return (
+    <div className="py-24 bg-white sm:py-32">
+      <div className="px-6 mx-auto max-w-7xl lg:px-8">
+        <div className="max-w-2xl mx-auto lg:max-w-4xl">
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+            From the blog
+          </h2>
+          <p className="mt-2 text-lg leading-8 text-gray-600">
+            Learn how to grow your business with our expert advice.
+          </p>
+          <div className="mt-16 space-y-20 lg:mt-20 lg:space-y-20">
+            {posts.map((post) => (
+              <article
+                key={post.id}
+                className="relative flex flex-col gap-8 isolate lg:flex-row"
+              >
+                <div className="relative aspect-[16/9] sm:aspect-[2/1] lg:aspect-square lg:w-64 lg:shrink-0">
+                  <img
+                    src={post.imageUrl}
+                    alt=""
+                    className="absolute inset-0 object-cover w-full h-full rounded-2xl bg-gray-50"
+                  />
+                  <div className="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
+                </div>
+                <div>
+                  <div className="flex items-center text-xs gap-x-4">
+                    <time dateTime={post.datetime} className="text-gray-500">
+                      {post.date}
+                    </time>
+                    <a
+                      href={post.category.href}
+                      className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
+                    >
+                      {post.category.title}
+                    </a>
+                  </div>
+                  <div className="relative max-w-xl group">
+                    <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+                      <a href={post.href}>
+                        <span className="absolute inset-0" />
+                        {post.title}
+                      </a>
+                    </h3>
+                    <p className="mt-5 text-sm leading-6 text-gray-600">
+                      {post.description}
+                    </p>
+                  </div>
+                  <div className="flex pt-6 mt-6 border-t border-gray-900/5">
+                    <div className="relative flex items-center gap-x-4">
+                      <img
+                        src={post.author.imageUrl}
+                        alt=""
+                        className="w-10 h-10 rounded-full bg-gray-50"
+                      />
+                      <div className="text-sm leading-6">
+                        <p className="font-semibold text-gray-900">
+                          <a href={post.author.href}>
+                            <span className="absolute inset-0" />
+                            {post.author.name}
+                          </a>
+                        </p>
+                        <p className="text-gray-600">{post.author.role}</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </article>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
