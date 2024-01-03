fa6fc8bb7e7c5d0ba0d7295bf33306e6546363f3
Marwen Arfa <marwen.arfa@gmail.com>
2024-01-03 07:54:44 +0100

    refactor: FrCounter component to accept props for initial value and maximum value

--- components/shared/FrCounter.tsx
+++ components/shared/FrCounter.tsx
@@ -3,18 +3,26 @@
 import { animate, motion, useMotionValue, useTransform } from 'framer-motion'
 import { useEffect } from 'react'
 
-export function FrCounter() {
-  const count = useMotionValue(0)
+interface FrCounterProps {
+  initial?: number
+  max?: number
+  className?: string
+}
+
+export function FrCounter({ initial = 0, className, max =50 }: FrCounterProps) {
+  const count = useMotionValue(initial)
   const rounded = useTransform(count, Math.round)
 
   useEffect(() => {
-    const animation = animate(count, 50, { duration: 2 })
+    const animation = animate(count, max, { duration: 2 })
 
     return animation.stop
   }, [])
 
   return (
-    <motion.h1 className="px-4 py-2 font-bold text-green-500 rounded w-18">
+    <motion.h1
+      className={`${className} px-4 py-2 font-bold text-green-500 rounded w-18`}
+    >
       {rounded}
     </motion.h1>
   )
