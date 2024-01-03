078a595d6f2f5c9b0fac0095c6668882dcc1f6d1
Marwen Arfa <marwen.arfa@gmail.com>
2024-01-03 08:55:33 +0100

    refactor footer and home page

--- components/global/Footer/FooterLayout.tsx
+++ components/global/Footer/FooterLayout.tsx
@@ -1,14 +1,7 @@
 import type { PortableTextBlock } from '@portabletext/types'
-import Link from 'next/link'
 
-import { CustomPortableText } from '@/components//shared/CustomPortableText'
-import { Icons } from '@/components/shared/icons'
-import { ModeToggleSelect } from '@/components/shared/mode-toggle-select'
-import { buttonVariants } from '@/components/ui/button'
-import { cn } from '@/lib/utils'
+import FooterDemo from '@/components/demos/FooterDemo'
 import type { MenuItem, SettingsPayload } from '@/types'
-
-import { MainNav } from '../Navbar/main-nav'
 
 interface FooterProps {
   data: SettingsPayload
@@ -18,45 +11,6 @@ export default function Footer(props: FooterProps) {
   const footer = data?.footer || ([] as PortableTextBlock[])
   const menuItems = data?.menuItems || ([] as MenuItem[])
   return (
-    <footer className="bottom-0 w-full mt-8 text-center text-white bg-green-500">
-      <div className="container py-4 md:py-8">
-        <div className="flex flex-col items-center justify-between md:flex-row">
-          <MainNav menuItems={menuItems} className="text-white" />
-          {footer && (
-            <CustomPortableText paragraphClasses="text-white" value={footer} />
-          )}
-
-          <nav className="flex items-center">
-            <Link href="#" target="_blank" rel="noreferrer">
-              <div
-                className={cn(
-                  buttonVariants({
-                    variant: 'ghost',
-                  }),
-                  'w-9 px-0',
-                )}
-              >
-                <Icons.google className="w-4 h-4" />
-                <span className="sr-only">Google</span>
-              </div>
-            </Link>
-            <Link href="#" target="_blank" rel="noreferrer">
-              <div
-                className={cn(
-                  buttonVariants({
-                    variant: 'ghost',
-                  }),
-                  'w-9 px-0',
-                )}
-              >
-                <Icons.twitter className="w-3 h-3 fill-current" />
-                <span className="sr-only">Twitter</span>
-              </div>
-            </Link>
-            <ModeToggleSelect />
-          </nav>
-        </div>
-      </div>
-    </footer>
+    <FooterDemo />
   )
 }
--- components/pages/home/HomePage.tsx
+++ components/pages/home/HomePage.tsx
@@ -1,26 +1,24 @@
 import { toPlainText } from '@portabletext/react'
 import type { EncodeDataAttributeCallback } from '@sanity/react-loader'
-import { Globe, Heart, User } from 'lucide-react'
 
 import { AccordionDemo } from '@/components/demos/AccordionDemo'
+import HeaderDemo from '@/components/demos/HeaderDemo'
+import { MasonryDemo } from '@/components/demos/MasonryDemo'
+import { MasonryDemo2 } from '@/components/demos/MasonryDemo2'
+import { MasonryDemo3 } from '@/components/demos/MasonryDemo3'
+import PostsDemo from '@/components/demos/PostsDemo'
+import SectionDemo from '@/components/demos/SectionDemo'
+import StatsDemo from '@/components/demos/StatsDemo'
+import StatsDemo2 from '@/components/demos/StatsDemo2'
+import TestimonialsDemo from '@/components/demos/TestimonialsDemo'
 import { ProjectListItem } from '@/components/pages/home/ProjectListItem'
 import BlockBenefits from '@/components/shared/BlockBenefits'
 import Bullet from '@/components/shared/Bullet'
-import { CardNavigation } from '@/components/shared/CardNavigation'
 import { CarouselReadMore } from '@/components/shared/CarouselReadMore'
-import { Header } from '@/components/shared/Header'
 import { bullets } from '@/lib/data'
 import type { HomePagePayload } from '@/types'
 
 import { HomePageProjects } from './HomePageProjects'
-import Image from 'next/image'
-import { MasonryDemo } from '@/components/demos/MasonryDemo'
-import { MasonryDemo2 } from '@/components/demos/MasonryDemo2'
-import { MasonryDemo3 } from '@/components/demos/MasonryDemo3'
-import SectionDemo from '@/components/demos/SectionDemo'
-import HeaderDemo from '@/components/demos/HeaderDemo'
-import StatsDemo from '@/components/demos/StatsDemo'
-import TestimonialsDemo from '@/components/demos/TestimonialsDemo'
 
 const width = 550
 const height = 280
@@ -72,6 +70,46 @@ export function HomePage({ data, encodeDataAttribute }: HomePageProps) {
         width={width}
         height={height}
       />
+      <div className="container">
+        <BlockBenefits
+          title="Providing Sustainable Energy Solutions"
+          desc="Nam vitae tortor quis est tempus egestas. Suspendisse non erat non mi imperdiet fringilla at vel ipsum. Proin rutrum, diam vel scelerisque luctus, leo dui sodales massa, et mattis urna felis quis mi turpis egestas."
+          image={{
+            src: '/images/background_01.jpg',
+            alt: 'background',
+          }}
+          imgPos="left"
+          secondaryNode={<MasonryDemo />}
+        >
+          <div className="w-full mt-5">
+            {bullets.map((item, index) => (
+              <Bullet key={index} title={item.title} icon={item.icon}>
+                {item.desc}
+              </Bullet>
+            ))}
+          </div>
+        </BlockBenefits>
+      </div>
+      <StatsDemo2 />
+      <SectionDemo />
+      <StatsDemo />
+      <PostsDemo />
+      <div className="container">
+        {/* AccordionDemo */}
+        <BlockBenefits
+          title="Providing Sustainable Energy Solutions"
+          desc="Nam vitae tortor quis est tempus egestas. Suspendisse non erat non mi imperdiet fringilla at vel ipsum. Proin rutrum, diam vel scelerisque luctus, leo dui sodales massa, et mattis urna felis quis mi turpis egestas."
+          image={{
+            src: '/images/background_01.jpg',
+            alt: 'background',
+          }}
+          imgPos="right"
+          secondaryNode={<MasonryDemo2 />}
+        >
+          <AccordionDemo />
+        </BlockBenefits>
+        <TestimonialsDemo />
+      </div>
     </div>
   )
 }
