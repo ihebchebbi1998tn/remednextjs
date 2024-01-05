b4ce00e0e662d8b36fa2a604e00b63e5dbeede5e
Marwen Arfa <marwen.arfa@gmail.com>
2024-01-05 04:17:08 +0100

    feat: add PageTitle component and its stories

---
+++ components/demos/PageTitle.tsx
@@ -0,0 +1,86 @@
+import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
+
+export function PageTitle() {
+  return (
+    <div className="min-w-[700px] pb-5 border-b border-gray-200">
+      <div>
+        <nav className="sm:hidden" aria-label="Back">
+          <a
+            href="#"
+            className="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700"
+          >
+            <ChevronLeftIcon
+              className="flex-shrink-0 w-5 h-5 mr-1 -ml-1 text-gray-400"
+              aria-hidden="true"
+            />
+            Back
+          </a>
+        </nav>
+        <nav className="hidden sm:flex" aria-label="Breadcrumb">
+          <ol role="list" className="flex items-center space-x-4">
+            <li>
+              <div className="flex">
+                <a
+                  href="#"
+                  className="text-sm font-medium text-gray-500 hover:text-gray-700"
+                >
+                  Jobs
+                </a>
+              </div>
+            </li>
+            <li>
+              <div className="flex items-center">
+                <ChevronRightIcon
+                  className="flex-shrink-0 w-5 h-5 text-gray-400"
+                  aria-hidden="true"
+                />
+                <a
+                  href="#"
+                  className="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700"
+                >
+                  Engineering
+                </a>
+              </div>
+            </li>
+            <li>
+              <div className="flex items-center">
+                <ChevronRightIcon
+                  className="flex-shrink-0 w-5 h-5 text-gray-400"
+                  aria-hidden="true"
+                />
+                <a
+                  href="#"
+                  aria-current="page"
+                  className="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700"
+                >
+                  Back End Developer
+                </a>
+              </div>
+            </li>
+          </ol>
+        </nav>
+      </div>
+      <div className="mt-2 md:flex md:items-center md:justify-between">
+        <div className="flex-1 min-w-0">
+          <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
+            Back End Developer
+          </h2>
+        </div>
+        <div className="flex flex-shrink-0 mt-4 md:ml-4 md:mt-0">
+          <button
+            type="button"
+            className="inline-flex items-center px-3 py-2 text-sm font-semibold text-gray-900 bg-white rounded-md shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+          >
+            Edit
+          </button>
+          <button
+            type="button"
+            className="inline-flex items-center px-3 py-2 ml-3 text-sm font-semibold text-white bg-indigo-600 rounded-md shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Publish
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
