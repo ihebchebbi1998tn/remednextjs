1cad4aa374351b4274fc3f3d6c378263ccf6f37a
Marwen Arfa <marwen.arfa@gmail.com>
2024-01-05 04:17:52 +0100

    feat: add BentoGrid component and stories

---
+++ components/ui/BentoGrid.stories.tsx
@@ -0,0 +1,42 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { BentoGrid, BentoGridItem } from './BentoGrid'
+import { items } from '@/lib/data'
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+  title: 'shared/framer/BentoGrid',
+  component: BentoGrid,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+    layout: 'centered',
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ['autodocs'],
+  // More on argTypes: https://storybook.js.org/docs/api/argtypes
+  argTypes: {},
+} satisfies Meta<typeof BentoGrid>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const Default: Story = {
+  args: {},
+  render: (props) => {
+    return (
+      <BentoGrid className="max-w-4xl mx-auto">
+        {items.map((item, i) => (
+          <BentoGridItem
+            key={i}
+            title={item.title}
+            description={item.description}
+            header={item.header}
+            icon={item.icon}
+            className={i === 3 || i === 6 ? 'md:col-span-2' : ''}
+          />
+        ))}
+      </BentoGrid>
+    )
+  },
+}
