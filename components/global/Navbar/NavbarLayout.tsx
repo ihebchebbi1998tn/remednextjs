07790b0645d2930fd59aa452879a28b545f6b573
Marwen Arfa <marwen.arfa@gmail.com>
2024-01-06 06:02:58 +0100

    refactor:  fix MainNav component in NavbarLayout

--- components/global/Navbar/NavbarLayout.tsx
+++ components/global/Navbar/NavbarLayout.tsx
@@ -1,36 +1,48 @@
+import Image from 'next/image'
 import Link from 'next/link'
 
 import { FrButton } from '@/components/shared/FrButton'
 /* import { CommandMenu } from '@/components/command-menu' */
-import { Icons } from '@/components/shared/icons'
 import { ModeToggle } from '@/components/shared/mode-toggle'
-import { Button } from '@/components/ui/button'
-import type { MenuItem, SettingsPayload } from '@/types'
+import { SocialNetworksList } from '@/components/shared/SocialNetworksList'
+import type { SettingsPayload } from '@/types'
 
 import { MainNav } from './main-nav'
-import { SocialNetworksList } from '@/components/shared/SocialNetworksList'
 
 interface NavbarProps {
   data: SettingsPayload
 }
 export default function Navbar(props: NavbarProps) {
   const { data } = props
-  const menuItems = data?.menuItems || [];
-  const internalLinks = data?.internalLinks || [];
-  const socialNetworks = data?.socialNetworks;
+  const menuItems = data?.menuItems || []
+  const internalLinks = data?.internalLinks || []
+  const socialNetworks = data?.socialNetworks
+  const homeItem = menuItems.find((item) => item?._type === 'home')
 
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex items-center h-16 max-w-screen-2xl">
-        <MainNav menuItems={menuItems} withLogo />
+      <div className="container flex items-center h-16 gap-x-8 max-w-screen-2xl">
+        <Link href="/" className="flex items-center gap-x-2">
+          <Image
+            className="w-10 h-10"
+            src="/images/logo.PNG"
+            alt={`Logo for ${homeItem?.title}`}
+            width="40"
+            height="40"
+          />
+          <span className="hidden font-bold sm:inline-block">
+            {homeItem?.title}
+          </span>
+        </Link>
+        <MainNav menuItems={menuItems} />
         <MainNav menuItems={internalLinks} />
         {/* <MobileNav /> */}
-        <div className="flex items-center justify-between flex-1 space-x-2 md:justify-end">
+        <div className="flex items-center justify-between flex-1 gap-x-2 md:justify-end">
           {/* <div className="flex-1 w-full md:w-auto md:flex-none">
             <CommandMenu />
           </div> */}
+          <FrButton />
           <nav className="flex items-center gap-2">
-            <FrButton />
             <SocialNetworksList socialNetworks={socialNetworks} />
             <ModeToggle />
           </nav>
--- components/global/Navbar/main-nav.tsx
+++ components/global/Navbar/main-nav.tsx
@@ -14,35 +14,18 @@ import type { MenuItem } from '@/types'
 interface Props {
   className?: string
   menuItems: MenuItem[]
-  withLogo?: boolean
 }
 
 export function MainNav(props: Props) {
   const pathname = usePathname()
 
-  const { menuItems, withLogo } = props
-
-  const homeItem = menuItems.find((item) => item?._type === 'home')
+  const { menuItems } = props
 
   const restItems = menuItems.filter((item) => item?._type !== 'home')
 
   return (
-    <div className="hidden mr-4 md:flex">
-      {withLogo && (
-        <Link href="/" className="flex items-center mr-6 space-x-2">
-          <Image
-            className="w-10 h-10"
-            src="/images/logo.PNG"
-            alt={`Logo for ${homeItem?.title}`}
-            width="40"
-            height="40"
-          />
-          <span className="hidden font-bold sm:inline-block">
-            {homeItem?.title ?? 'New York'}
-          </span>
-        </Link>
-      )}
-      <nav className="flex items-center gap-6 text-sm">
+    <div className="hidden md:flex">
+      <nav className="flex items-center gap-8 text-sm">
         {restItems.map((item, key) => {
           const href = resolveHref(item?._type, item?.slug)
           if (!href) {
