13de054fbabd39818365ff0b975d4b7ac18b16aa
Marwen Arfa <marwen.arfa@gmail.com>
2024-01-19 22:41:34 +0100

    fix: optional chaining in Testimonials component

--- components/demos/Testimonials.tsx
+++ components/demos/Testimonials.tsx
@@ -61,7 +61,7 @@ export function Testimonials({
         <div className="grid max-w-2xl grid-cols-1 grid-rows-1 gap-8 mx-auto mt-16 text-sm leading-6 text-gray-900 sm:mt-20 sm:grid-cols-2 xl:mx-0 xl:max-w-none xl:grid-flow-col xl:grid-cols-4 ">
           <figure className="bg-white shadow-lg rounded-2xl ring-1 ring-gray-900/5 sm:col-span-2 xl:col-start-2 xl:row-end-1">
             <blockquote className="p-6 text-lg font-semibold leading-7 tracking-tight text-gray-900 sm:p-12 sm:text-xl sm:leading-8">
-              <p>{`“${featuredTestimonial.body}”`}</p>
+              <p>{`“${featuredTestimonial?.body}”`}</p>
             </blockquote>
             <figcaption className="flex flex-wrap items-center px-6 py-4 border-t gap-x-4 gap-y-4 border-gray-900/10 sm:flex-nowrap">
               <img
@@ -101,23 +101,23 @@ export function Testimonials({
                 >
                   {column.map((testimonial) => (
                     <figure
-                      key={testimonial.author.handle}
+                      key={testimonial?.author?.handle}
                       className="p-6 bg-white shadow-lg rounded-2xl ring-1 ring-gray-900/5"
                     >
                       <blockquote className="text-gray-900">
-                        <p>{`“${testimonial.body}”`}</p>
+                        <p>{`“${testimonial?.body}”`}</p>
                       </blockquote>
                       <figcaption className="flex items-center mt-6 gap-x-4">
                         <img
                           className="w-10 h-10 rounded-full bg-gray-50"
-                          src={testimonial.author.imageUrl}
+                          src={testimonial?.author?.imageUrl}
                           alt=""
                         />
                         <div>
                           <div className="font-semibold">
-                            {testimonial.author.name}
+                            {testimonial?.author?.name}
                           </div>
-                          <div className="text-gray-600">{`@${testimonial.author.handle}`}</div>
+                          <div className="text-gray-600">{`@${testimonial?.author?.handle}`}</div>
                         </div>
                       </figcaption>
                     </figure>
