7ee9db5baa096b72bb410f14f91ce7734629e859
Marwen Arfa <marwen.arfa@gmail.com>
2024-01-20 20:21:10 +0100

    feat: add certifications field to project schema

--- sanity/schemas/documents/project.ts
+++ sanity/schemas/documents/project.ts
@@ -152,5 +152,18 @@ export default defineType({
         }),
       ],
     }),
+    defineField({
+      name: 'certifications',
+      title: 'Certifications',
+      description:
+        'This field is used to display the certification for the project.',
+      type: 'array',
+      of: [
+        defineArrayMember({
+          type: 'reference',
+          to: [{ type: 'certification' }],
+        }),
+      ],
+    }),
   ],
 })
