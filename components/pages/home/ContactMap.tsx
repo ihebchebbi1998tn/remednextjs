0304a748552b4dc1331eeabc9bb80abf9d64b974
Marwen Arfa <marwen.arfa@gmail.com>
2024-01-24 21:03:16 +0100

    refactor: use an array of locations on ContactMap component

--- components/pages/home/ContactMap.tsx
+++ components/pages/home/ContactMap.tsx
@@ -2,28 +2,26 @@
 
 import 'mapbox-gl/dist/mapbox-gl.css'
 
-import { useState } from 'react'
 import { Map, Marker, Popup } from 'react-map-gl'
 
-export interface ContactMapProps {
-  latitude?: number
-  longitude?: number
-  zoom?: number
-}
+const LOCATIONS = [
+  {
+    latitude: 36.7534865,
+    longitude: 10.2780854,
+  },
+  {
+    latitude: 36.8574293,
+    longitude: 10.1548072,
+  },
+]
 
-export function ContactMap({
-  latitude = 36.7534865,
-  longitude = 10.2780854,
-  zoom = 14,
-}: ContactMapProps) {
-  const [showPopup, setShowPopup] = useState<boolean>(true)
-
+export function ContactMap() {
   return (
     <Map
       initialViewState={{
-        latitude,
-        longitude,
-        zoom,
+        latitude: LOCATIONS[0].latitude,
+        longitude: LOCATIONS[0].longitude,
+        zoom: 10,
       }}
       style={{ minWidth: 300, height: 480 }}
       mapStyle="mapbox://styles/mapbox/streets-v9"
@@ -32,32 +30,49 @@ export function ContactMap({
       }
     >
       <Marker
-        latitude={latitude}
-        longitude={longitude}
-        onClick={() => setShowPopup((showPopup) => !showPopup)}
+        latitude={LOCATIONS[0].latitude}
+        longitude={LOCATIONS[0].longitude}
         color="hsl(var(--reg-main))"
       />
-      {showPopup && (
-        <Popup
-          latitude={latitude}
-          longitude={longitude}
-          closeButton={false}
-          closeOnClick={false}
-          onClose={() => setShowPopup(false)}
-          anchor="top"
-        >
-          <div className="text-black">
-            <h3 className="text-sm font-semibold">
-              Respect Environnement Group
-            </h3>
-            <p className="text-xs">
-              Ezzahra Plage, Lotissement Afh
-              <br/>
-              Ben Arous Ezzahra, Tunisie
-            </p>
-          </div>
-        </Popup>
-      )}
+      <Marker
+        latitude={LOCATIONS[1].latitude}
+        longitude={LOCATIONS[1].longitude}
+        color="hsl(var(--reg-main))"
+      />
+      <Popup
+        latitude={LOCATIONS[0].latitude}
+        longitude={LOCATIONS[0].longitude}
+        closeButton={false}
+        closeOnClick={false}
+        anchor="top"
+      >
+        <div className="text-black">
+          <h3 className="text-sm font-semibold">Respect Environnement Group</h3>
+          <p className="text-xs">
+            Ezzahra Plage, Lotissement Afh
+            <br />
+            Ben Arous Ezzahra, Tunisie
+          </p>
+        </div>
+      </Popup>
+      <Popup
+        latitude={LOCATIONS[1].latitude}
+        longitude={LOCATIONS[1].longitude}
+        closeButton={false}
+        closeOnClick={false}
+        anchor="top"
+      >
+        <div className="text-black">
+          <h3 className="text-sm font-semibold">
+            Respect environnement group Tunis
+          </h3>
+          <p className="text-xs">
+            1ére étage, Bloc A, Résidence Essalem
+            <br />
+            Ariana 2037, Tunisia
+          </p>
+        </div>
+      </Popup>
     </Map>
   )
 }
