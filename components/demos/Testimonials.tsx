54aa8ad144526918142a1ece5dc9f333382c53e3
Marwen Arfa <marwen.arfa@gmail.com>
2024-02-13 06:54:10 +0100

    fix: testimonials

--- components/demos/Testimonials.tsx
+++ components/demos/Testimonials.tsx
@@ -61,7 +61,7 @@ export function Testimonials({
         <div className="grid max-w-2xl grid-cols-1 grid-rows-1 gap-8 mx-auto mt-16 text-sm leading-6 text-gray-900 sm:mt-20 sm:grid-cols-2 xl:mx-0 xl:max-w-none xl:grid-flow-col xl:grid-cols-4 ">
           <figure className="bg-white shadow-lg rounded-2xl ring-1 ring-gray-900/5 sm:col-span-2 xl:col-start-2 xl:row-end-1">
             <blockquote className="p-6 text-lg font-semibold leading-7 tracking-tight text-gray-900 sm:p-12 sm:text-xl sm:leading-8">
-              <p>{`“${featuredTestimonial?.body}”`}</p>
+              <p>{featuredTestimonial?.body}</p>
             </blockquote>
             <figcaption className="flex flex-wrap items-center px-6 py-4 border-t gap-x-4 gap-y-4 border-gray-900/10 sm:flex-nowrap">
               <img
@@ -105,7 +105,7 @@ export function Testimonials({
                       className="p-6 bg-white shadow-lg rounded-2xl ring-1 ring-gray-900/5"
                     >
                       <blockquote className="text-gray-900">
-                        <p>{`“${testimonial?.body}”`}</p>
+                        <p>{testimonial?.body}</p>
                       </blockquote>
                       <figcaption className="flex items-center mt-6 gap-x-4">
                         <img
