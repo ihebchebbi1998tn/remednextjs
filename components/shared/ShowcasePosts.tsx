2cb5d2a894814acf8ab0e98d0aca74aacb417be5
Marwen Arfa <marwen.arfa@gmail.com>
2024-03-29 01:32:03 +0100

    feat: add AspectRatio shadcn ui component

--- components/shared/ShowcasePosts.tsx
+++ components/shared/ShowcasePosts.tsx
@@ -1,4 +1,6 @@
-import Image from "next/image"
+import Image from 'next/image'
+
+import { AspectRatio } from '@/components/ui/aspect-ratio'
 
 /* eslint-disable @next/next/no-img-element */
 interface Block {
@@ -18,11 +20,10 @@ export function ShowcasePosts({
   description,
   blocks,
 }: ShowcasePostsProps) {
-
   return (
     <>
       <div className="container relative flex flex-col justify-between h-full max-w-6xl px-10 mx-auto mt-5 xl:px-0">
-        <h2 className="mb-1 text-3xl font-extrabold leading-tight text-gray-900">
+        <h2 className="mt-2 mb-1 text-3xl font-bold leading-tight tracking-tight text-gray-800 sm:text-4xl dark:text-gray-200">
           {title}
         </h2>
         <p className="mb-12 text-lg text-gray-500">{description}</p>
@@ -32,13 +33,17 @@ export function ShowcasePosts({
               <div className="relative h-full ml-0 mr-0 sm:mr-10">
                 <span className="absolute top-0 left-0 w-full h-full mt-1 ml-1 bg-indigo-500 rounded-lg"></span>
                 <div className="relative h-full p-5 bg-white border-2 border-indigo-500 rounded-lg">
-                  <Image
-                    src={blocks?.[0].coverImage || ''} 
-                    width={400}
-                    height={300}
-                    alt={blocks?.[0].title || ''}
-                  />
-                  <div className="flex items-center -mt-1">
+                  <div className="mb-3">
+                    <AspectRatio ratio={16 / 9}>
+                      <Image
+                        className="object-cover"
+                        fill
+                        src={blocks?.[0].coverImage || ''}
+                        alt={blocks?.[0].title || ''}
+                      />
+                    </AspectRatio>
+                  </div>
+                  <div className="flex items-center ">
                     <h3 className="my-2 ml-3 text-lg font-bold text-gray-800">
                       {blocks?.[0].title}
                     </h3>
@@ -56,7 +61,17 @@ export function ShowcasePosts({
               <div className="relative h-full ml-0 md:mr-10">
                 <span className="absolute top-0 left-0 w-full h-full mt-1 ml-1 bg-purple-500 rounded-lg"></span>
                 <div className="relative h-full p-5 bg-white border-2 border-purple-500 rounded-lg">
-                  <div className="flex items-center -mt-1">
+                  <div className="mb-3">
+                    <AspectRatio ratio={16 / 9}>
+                      <Image
+                        className="object-cover"
+                        fill
+                        src={blocks?.[1].coverImage || ''}
+                        alt={blocks?.[1].title || ''}
+                      />
+                    </AspectRatio>
+                  </div>
+                  <div className="flex items-center ">
                     <h3 className="my-2 ml-3 text-lg font-bold text-gray-800">
                       {blocks?.[1].title}
                     </h3>
@@ -76,7 +91,17 @@ export function ShowcasePosts({
               <div className="relative h-full ml-0 mr-0 sm:mr-10">
                 <span className="absolute top-0 left-0 w-full h-full mt-1 ml-1 bg-blue-400 rounded-lg"></span>
                 <div className="relative h-full p-5 bg-white border-2 border-blue-400 rounded-lg">
-                  <div className="flex items-center -mt-1">
+                  <div className="mb-3">
+                    <AspectRatio ratio={16 / 9}>
+                      <Image
+                        className="object-cover"
+                        fill
+                        src={blocks?.[2].coverImage || ''}
+                        alt={blocks?.[2].title || ''}
+                      />
+                    </AspectRatio>
+                  </div>
+                  <div className="flex items-center ">
                     <h3 className="my-2 ml-3 text-lg font-bold text-gray-800">
                       {blocks?.[2].title}
                     </h3>
@@ -94,7 +119,17 @@ export function ShowcasePosts({
               <div className="relative h-full ml-0 mr-0 sm:mr-10">
                 <span className="absolute top-0 left-0 w-full h-full mt-1 ml-1 bg-yellow-400 rounded-lg"></span>
                 <div className="relative h-full p-5 bg-white border-2 border-yellow-400 rounded-lg">
-                  <div className="flex items-center -mt-1">
+                  <div className="mb-3">
+                    <AspectRatio ratio={16 / 9}>
+                      <Image
+                        className="object-cover"
+                        fill
+                        src={blocks?.[3].coverImage || ''}
+                        alt={blocks?.[3].title || ''}
+                      />
+                    </AspectRatio>
+                  </div>
+                  <div className="flex items-center ">
                     <h3 className="my-2 ml-3 text-lg font-bold text-gray-800">
                       {blocks?.[3].title}
                     </h3>
@@ -112,7 +147,17 @@ export function ShowcasePosts({
               <div className="relative h-full ml-0 md:mr-10">
                 <span className="absolute top-0 left-0 w-full h-full mt-1 ml-1 bg-green-500 rounded-lg"></span>
                 <div className="relative h-full p-5 bg-white border-2 border-green-500 rounded-lg">
-                  <div className="flex items-center -mt-1">
+                  <div className="mb-3">
+                    <AspectRatio ratio={16 / 9}>
+                      <Image
+                        className="object-cover"
+                        fill
+                        src={blocks?.[4].coverImage || ''}
+                        alt={blocks?.[4].title || ''}
+                      />
+                    </AspectRatio>
+                  </div>
+                  <div className="flex items-center ">
                     <h3 className="my-2 ml-3 text-lg font-bold text-gray-800">
                       {blocks?.[4].title}
                     </h3>
