d1ac79288ea02c5c48c65a48e2b1061dddbb2089
Marwen Arfa <marwen.arfa@gmail.com>
2024-04-29 11:33:10 +0100

    fix: FrCounter to start when in view

--- components/shared/FrCounter.tsx
+++ components/shared/FrCounter.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 'use client'
-import { animate, motion, useMotionValue, useTransform } from 'framer-motion'
-import { useEffect } from 'react'
+import { animate, motion, useInView, useMotionValue, useTransform } from 'framer-motion'
+import { useEffect, useRef } from 'react'
 
 interface FrCounterProps {
   start?: number
@@ -12,18 +12,21 @@ interface FrCounterProps {
 export function FrCounter({ start = 0, className, max = 50 }: FrCounterProps) {
   const count = useMotionValue(start)
   const rounded = useTransform(count, Math.round)
-
+  const ref = useRef<HTMLSpanElement>(null)
+  const isInView = useInView(ref, { once: true, margin: '-100px' })
   useEffect(() => {
+    if (!isInView) return
     const animation = animate(count, max, { duration: 2 })
 
     return animation.stop
-  }, [])
+  }, [isInView, count, max])
 
   return (
-    <motion.h1
-      className={`${className} px-4 py-2 font-bold text-green-500 rounded w-18`}
+    <motion.span
+      className={`${className} px-2`}
+      ref={ref}
     >
       {rounded}
-    </motion.h1>
+    </motion.span>
   )
 }
